097fc30ebf272140f532ed80d830cc116dcaac7c
Julio Ruiz Villavicencio <julio.ruiz@utec.edu.pe>
2025-05-06 17:28:51 -0500

    agregando CompanyRepository.java

--- Hackathon/src/main/java/com/example/hackathon/Repository/CompanyRepository.java
+++ Hackathon/src/main/java/com/example/hackathon/Repository/CompanyRepository.java
@@ -1,4 +1,15 @@
-package com.example.hackathon.Repository;
+package com.sparkyconsulting.aihub.repository;
 
-public interface CompanyRepository {
+import com.sparkyconsulting.aihub.model.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByName(String name);
+    Optional<Company> findByRuc(String ruc);
+    boolean existsByName(String name);
+    boolean existsByRuc(String ruc);
 }

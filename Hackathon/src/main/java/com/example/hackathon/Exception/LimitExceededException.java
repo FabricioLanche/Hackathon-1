7c4db4be694bf44973390d15d258d104c2572af7
Alessandromg18 <alessandro.monzon@utec.edu.pe>
2025-05-06 17:37:21 -0500

    fest LimitExceededException : Modifique el exception

--- Hackathon/src/main/java/com/example/hackathon/Exception/LimitExceededException.java
+++ Hackathon/src/main/java/com/example/hackathon/Exception/LimitExceededException.java
@@ -1,7 +1,34 @@
-package com.example.hackathon.Exception;
+package com.sparkyconsulting.aihub.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class LimitExceededException extends RuntimeException {
-    public LimitExceededException(String message) {
-        super(message);
+    
+    private static final long serialVersionUID = 1L;
+    
+    private String limitType;
+    private long currentUsage;
+    private long maxLimit;
+    
+    public LimitExceededException(String limitType, long currentUsage, long maxLimit) {
+        super(String.format("%s limit exceeded: current usage is %d, maximum allowed is %d", 
+                           limitType, currentUsage, maxLimit));
+        this.limitType = limitType;
+        this.currentUsage = currentUsage;
+        this.maxLimit = maxLimit;
+    }
+    
+    public String getLimitType() {
+        return limitType;
+    }
+    
+    public long getCurrentUsage() {
+        return currentUsage;
+    }
+    
+    public long getMaxLimit() {
+        return maxLimit;
     }
 }

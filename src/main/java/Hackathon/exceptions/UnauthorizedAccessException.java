8193ad1cd1bdd0a8214cb33385c570f78f80c822
Alessandromg18 <alessandro.monzon@utec.edu.pe>
2025-05-07 22:54:41 -0500

    feat: Se añadio UnauthorizedAccessException.java

---
+++ src/main/java/Hackathon/exceptions/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package Hackathon.exceptions;
+
+public class UnauthorizedAccessException extends RuntimeException {
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
+}
